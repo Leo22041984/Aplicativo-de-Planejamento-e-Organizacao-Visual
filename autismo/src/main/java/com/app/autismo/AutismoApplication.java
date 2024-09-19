9df88f7420b44d72c14d1a04454d4ffc8bf6878f
Leonardo Quintino de Oliveira <125229356+Leo22041984@users.noreply.github.com>
2024-09-18 23:02:16 -0300

    Commit Inicial
    Foi criada inicialmente a estrutura base do projeto, utilizamos a IDE NetBeans, utilizamos para a persistência dos dados o MYSQ WORKBENQ , utilizamos na construção do Projeto inicialmente Spring Boot, HTML5, CSS3, BootStrap, JavaScript, Thymeleaf,  foram construídos os pacotes para regras de negócios, Controller, Model, Service e Reposítory.

---
+++ autismo/src/main/java/com/app/autismo/AutismoApplication.java
@@ -0,0 +1,13 @@
+package com.app.autismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutismoApplication.class, args);
+	}
+
+}
---
+++ autismo/src/test/java/com/app/autismo/AutismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.autismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
